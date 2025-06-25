657e2b254592ecd829698117e4c3a0241d9f8e10
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-25 11:42:39 +0900

    .

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from .views import *
 urlpatterns = [    
     path('', index, name='index'),
-    path('detail/<int:pk>', detail, name='detail')
+    path('detail', detail, name='detail')
 ]
