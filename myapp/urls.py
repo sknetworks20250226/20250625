0fc8080fda38fb1ed726ba65984d895e6f76ff7f
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-25 11:16:41 +0900

    .

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import *
 urlpatterns = [    
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('detail/<int:pk>', detail, name='detail')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import JsonResponse
 # Create your views here.
 def index(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request,'myapp/index.html',{'posts':posts})
+
+def detail(request):
+    id = request.POST.get('id')
+    try:    
+        post = Post.objects.get(id=id)
+        content = post.content
+        return JsonResponse({'content':content})
+    except Post.DoesNotExist:
+        return JsonResponse({'error': '게시글이 존재하지 않습니다.'}, status=404)
